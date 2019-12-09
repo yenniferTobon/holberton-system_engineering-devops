98153d1fd21da5483108b43f36dd00fbd2ae1dfa
yenniferTobon <ymtobony@unal.edu.co>
2019-12-09 21:12:39 +0000

    0-create_a_file.pp Using Puppet, create a file in /tmp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # Using Puppet, create a file in /tmp
-
-file { "/createFile":
-	path => "/tmp/holberton",
-	mode => "0744",
-	owner => "www-data",
-	group => "www-data",
-	content => "I love Puppet",
+file { '/createFile':
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
