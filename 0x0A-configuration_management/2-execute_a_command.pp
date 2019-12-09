0766cd805e8eafcd8035cfee2c4875f6531d3bcb
yenniferTobon <ymtobony@unal.edu.co>
2019-12-09 21:14:09 +0000

    2-execute_a_command.pp create a manifest that kills a process named killmenow

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
 # create a manifest that kills a process named killmenow
-
 exec { 'pkil_process':
-	command => "pkill -f killmenow",
-	path => "/usr/bin", 
+  command => 'pkill -f killmenow',
+  path    => '/usr/bin'
 }
