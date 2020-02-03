3953f2520295f8ce5818b2679fc1a33472184cde
yenniferTobon <ymtobony@unal.edu.co>
2020-02-03 18:31:34 -0500

    0-gather_data_from_an_API.py requests to an api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,13 +8,13 @@ import requests
 if __name__ == "__main__":
     """funtion to requests to an api"""
     url = "https://jsonplaceholder.typicode.com/"
-    responseUser = requests.get(url + 'users/' + argv[1]).json()
+    responseUser = (requests.get(url + 'users/' + argv[1]).json()).get('name')
     responseTask = len(requests.get(url + 'todos?userId=' + argv[1]).json())
     responseTaskDone = requests.get(url + "todos?userId={}&&completed=true".
                                     format(argv[1])).json()
 
     print("Employee {} is done with tasks({}/{}):".
-          format(responseUser['name'], len(responseTaskDone), responseTask))
+          format(responseUser, len(responseTaskDone), responseTask))
 
     for element in responseTaskDone:
-        print("\t", element['title'])
+        print("     ", element['title'])
