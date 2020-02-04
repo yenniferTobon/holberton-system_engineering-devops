cbac25d5f4d574d6df2a92b27ae9401a4cb55f25
yenniferTobon <ymtobony@unal.edu.co>
2020-02-03 23:32:33 -0500

    2-export_to_JSON.py script to export data in the JSON format

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""script to export data in the JSON format"""
+
+import csv
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """script to export data in the JSON format"""
+    url = "https://jsonplaceholder.typicode.com/"
+    output = {}
+    listTask = []
+    responseUser = requests.get(url + 'users/' + argv[1]).json()
+    responseTask = requests.get(url + 'todos?userId=' + argv[1]).json()
+
+    for element in responseTask:
+        listTask.append({'task': element['title'],
+                         'completed': element['completed'],
+                         'username': responseUser['username']})
+
+    output = {"2": listTask}
+
+    nameFile = argv[1] + ".json"
+    myFile = open(nameFile, 'w')
+    with myFile:
+        json.dump(output, myFile)
