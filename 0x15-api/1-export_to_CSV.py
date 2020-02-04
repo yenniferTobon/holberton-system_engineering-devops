7d23f3510729bcff40471c05ca871e90b225d281
yenniferTobon <ymtobony@unal.edu.co>
2020-02-03 21:50:15 -0500

    script to export data in the CSV format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""script to export data in the CSV format"""
+import csv
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """script to export data in the CSV format"""
+    url = "https://jsonplaceholder.typicode.com/"
+    nameFile = ""
+    responseUser = requests.get(url + 'users/' + argv[1]).json()
+    responseTask = requests.get(url + 'todos?userId=' + argv[1]).json()
+
+    nameFile = argv[1] + ".csv"
+    myFile = open(nameFile, 'w')
+    with myFile:
+        writer = csv.writer(myFile, quoting=csv.QUOTE_ALL)
+        for element in responseTask:
+            writer.writerow([responseUser['id'], responseUser['username'],
+                            element['completed'], element['title']])
