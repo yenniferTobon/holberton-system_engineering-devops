8cfe9ffb11f1e2978092fdaad4bfa7ee988128e1
yenniferTobon <ymtobony@unal.edu.co>
2020-02-06 23:18:02 -0500

    README.md

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,15 +7,15 @@ import json
 def number_of_subscribers(subreddit):
     """returns the number of subscribers"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-
-    headers = {
-        'User-Agent': 'My User Agent 1.0'
-    }
-    
     response = requests.get(url)
     l = response.json()
 
-    try:
-        return (l.get('data').get('subscribers'))
-    except:
+    if response.status_code == 200:
+        for key, value in l.items():
+            if key == "data":
+                print("yenn")
+                for i, j in value.items():
+                    if i == 'subscribers':
+                        return (j)
+    else:
         return(0)
