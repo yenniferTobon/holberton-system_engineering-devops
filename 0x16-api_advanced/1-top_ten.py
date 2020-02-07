87618af69053215db942ec5d6fded9988fc923bf
yenniferTobon <ymtobony@unal.edu.co>
2020-02-07 00:01:58 -0500

    1-top_ten.py prints the titles of the first 10 hot posts

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,12 +8,12 @@ def top_ten(subreddit):
     """prints the titles of the first 10 hot posts"""
     url = 'https://api.reddit.com/r/{}?sort=hot&limit=10'.format(subreddit)
 
-    response = requests.get(url)
-    l = response.json()
-
     headers = {
-        'User-Agent': "My User Agent 1.0"
+        'User-Agent': 'My User Agent 1.0'
     }
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    l = response.json()
 
     if response.status_code == 200:
         result = l.get('data').get('children')
