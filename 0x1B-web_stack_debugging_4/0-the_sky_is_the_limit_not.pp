87960d0f9a572f8f9d3c4d5310161355e79c7f12
yenniferTobon <ymtobony@unal.edu.co>
2020-02-28 16:59:54 -0500

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
-# will make 2000 requests to my server with 100 requests
-exec 'change size the requests': {
-	command => '/bin/sed -i "s/15/30000/g" /etc/default/nginx | service nginx restart'
-     }
+# will make requests to my server
+exec { 'change size the requests':
+    command => '/bin/sed -i "s/15/30000/g" /etc/default/nginx | service nginx restart'
+}
