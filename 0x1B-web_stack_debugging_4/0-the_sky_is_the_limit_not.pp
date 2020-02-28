8b4225692cf3da745ba10b3be4c9fce231d52844
yenniferTobon <ymtobony@unal.edu.co>
2020-02-28 16:54:46 -0500

    0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,4 @@
+# will make 2000 requests to my server with 100 requests
+exec 'change size the requests': {
+	command => '/bin/sed -i "s/15/30000/g" /etc/default/nginx | service nginx restart'
+     }
